a7632134df0cd8c7593b3ca1baa1ee4bdf4e0429
biancaaa74 <91210580+biancaaa74@users.noreply.github.com>
2022-01-24 20:07:53 -0500

    myInfo version 2

--- myInfo.py
+++ myInfo.py
@@ -3,3 +3,8 @@
 #myInfo
 print("My name is Bianca")
 print("I am a student")
+print("My address is 437 Cedar Ave, West Long Branch, NJ 07764")
+print("My phone number is 609-456-7433")
+print("My major at Monmouth is Computer Science")
+
+      
