7a44a67f2e5e7b6391f50289bab9243997ed9ac9
biancaaa74 <91210580+biancaaa74@users.noreply.github.com>
2022-02-02 22:16:18 -0500

    stocks.py

---
+++ stocks.py
@@ -0,0 +1,14 @@
+# Bianca Beltran
+# stocks.py
+
+
+COMISSION_RATE = float(input('What was the commission rate?:'))
+NUM_SHARES = float(input('How many shares did you purchase?:'))
+PURCHASE_PRICE = float(input('What was the purchase price?:'))
+SELLING_PRICE = float(input('What was the selling price?:'))
+print('Amount paid for stock:',NUM_SHARES * PURCHASE_PRICE)
+AMOUNT_PAID = (NUM_SHARES * PURCHASE_PRICE)
+print('Comission paid on the purchase:', COMISSION_RATE * AMOUNT_PAID)
+
+print('Amount the stock sold for: 85500.0')
+
