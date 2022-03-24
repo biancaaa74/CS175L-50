f5f0a47337c78d13f85bc3fbde00d77d3be1af20
biancaaa74 <91210580+biancaaa74@users.noreply.github.com>
2022-03-24 13:55:35 -0400

    Add files via upload

--- average_from_file.py
+++ average_from_file.py
@@ -1,14 +1,26 @@
 #average_from_inputfile
 
-numbersfile = open('numbers.txt', 'r')
+import sys
+def main():
+    try:
+        numbersfile = open('numbers.txt', 'r')
+    except IOError:
+        print('Error file cant be read')
+        sys.exit()
+    number_sum = 0
+    counter = 0
+    
+    for line in numbersfile:
+        counter = counter +1
+        try:
+            number_sum += float(line)
+    
 
-number_sum = 0
-counter = 0
+            print(f'I read in {counter} number(s) Current number is {float(line)} is {number_sum}')
+        except ValueError:
+            print('Error, bad value: skipping')
+            counter = counter -1
+            continue
+        print('Average: ', number_sum / counter)
 
-for line in numbersfile:
-    counter = counter +1
-    number_sum += float(line)
-
-    print(f'I read in {counter} number(s) Current number is {float(line)} is {number_sum}')
-
-print('Average: ', number_sum / counter)
+main()
